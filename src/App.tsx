299037de281229a001f1eb6af767d3ed5f9c3f41
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-27 11:39:17 +0500

    setup navbar for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Nav from './Components/Nav';
 
 function App() {
   return (
-    <div>
-      Hey Buddies!!
+    <div className='App'>
+      <Router>
+        <Nav/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
