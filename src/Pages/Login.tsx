d357a00f789351ad5345ff8a5c73c2af523b49e9
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-28 15:11:27 +0500

    Updated login with Google functionality

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { auth, provider } from '../Config/firebase'
+import { signInWithPopup } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const navigate = useNavigate()
+
+    const googleSignin = async()=>{
+      const result = await signInWithPopup(auth, provider)
+      console.log(result);
+      navigate('/')
+    }
   return (
-    <div>Login</div>
+    <div>
+      <h1>Login</h1>
+        <button onClick={googleSignin}>SignIn with Google</button>
+    </div>
   )
 }
 
