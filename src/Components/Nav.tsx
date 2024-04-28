9292b55b2624443a83009c3722097cf493c53d9c
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-28 15:25:27 +0500

    installed react-firebase-hooks displayed logged-in user details

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { auth } from '../Config/firebase'
+import {useAuthState} from 'react-firebase-hooks/auth'
 
 const Nav = () => {
+  const [user] = useAuthState(auth)
   return (
     <>
     <Link to='/'>Home</Link>
     <Link to='/login'>Login</Link>
+    <div>
+      <p>{user?.displayName}</p>
+      <img src={user?.photoURL || 'profile picture'}/>
+    </div>
     </>
   )
 }
