cfffe92d72c7ae0b786b7eaf99358d2775e13863
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-28 15:35:50 +0500

    added logout functionality

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { auth } from '../Config/firebase'
-import {useAuthState} from 'react-firebase-hooks/auth'
+import React from "react";
+import { Link } from "react-router-dom";
+import { auth } from "../Config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 const Nav = () => {
-  const [user] = useAuthState(auth)
+  const [user] = useAuthState(auth);
+  const logout = async () => {
+    await signOut(auth);
+  };
   return (
     <>
-    <Link to='/'>Home</Link>
-    <Link to='/login'>Login</Link>
-    <div>
-      <p>{user?.displayName}</p>
-      <img src={user?.photoURL || 'profile picture'}/>
-    </div>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <div>
+        {user && (
+          <>
+            <p>{user?.displayName}</p>
+            <img src={user?.photoURL || "profile picture"} />
+            <br />
+            <button onClick={logout}>Log Out</button>
+          </>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
