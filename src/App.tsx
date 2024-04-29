e43ccf94bec2fecedac821a78c27999f8115155e
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-30 01:06:34 +0500

    Organized post creation files in CreatePost folder

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Nav from './Components/Nav';
-import CreatePost from './Pages/CreatePost';
+import CreatePost from './Pages/CreatePost/CreatePost';
 
 function App() {
   return (
