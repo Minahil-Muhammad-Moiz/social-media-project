6317d4f4edffa01e2ae2ecefc7e7efc9ceabac2b
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-05-01 00:28:37 +0500

    Added interface for type + error message display

--- src/Pages/CreatePost/CreatePostForm.tsx
+++ src/Pages/CreatePost/CreatePostForm.tsx
@@ -1,31 +1,44 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useForm } from "react-hook-form";
 import * as yup from "yup";
-import {yupResolver} from '@hookform/resolvers/yup'
+import { yupResolver } from "@hookform/resolvers/yup";
 
+interface FormData {
+  title: string;
+  description: string;
+}
 
 const CreatePostForm = () => {
-    const schema = yup.object().shape({
-        title: yup.string().required("Title is required"),
-        description: yup.string().required("Description is required"),
-    })
+  const schema = yup.object().shape({
+    title: yup.string().required("Title is required"),
+    description: yup.string().required("Description is required"),
+  });
 
-    const {register, handleSubmit} = useForm({
-        resolver: yupResolver(schema)
-    })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
 
-    const onPostDone = (data: any)=>{
-        console.log(data);
-        
-    }
+  const onPostDone = (data: FormData) => {
+    console.log(data);
+  };
 
   return (
     <form onSubmit={handleSubmit(onPostDone)}>
-        <input placeholder='Title...' {...register('title')} />
-        <textarea placeholder='Description...' {...register('description')}/>
-        <input type="submit" />
+      <input placeholder="Title..." {...register("title")} />
+      <p style={{ color: "red", fontStyle: "italic" }}>
+        {errors.title?.message}
+      </p>
+      <textarea placeholder="Description..." {...register("description")} />
+      <p style={{ color: "red", fontStyle: "italic" }}>
+        {errors.description?.message}
+      </p>
+      <input type="submit" />
     </form>
-  )
-}
+  );
+};
 
-export default CreatePostForm
+export default CreatePostForm;
