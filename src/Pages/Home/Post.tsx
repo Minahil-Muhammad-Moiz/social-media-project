6bbd1643e0d9c29b6707ffa5509e393ecdd55caf
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-05-06 17:20:01 +0500

    updated userID in the databse so a user can only like once from one account

--- src/Pages/Home/Post.tsx
+++ src/Pages/Home/Post.tsx
@@ -1,35 +1,59 @@
 import React, { useEffect, useState } from "react";
 import { IPost } from "./Home";
-import { addDoc, collection, doc, getDocs, query, where } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import { auth, database } from "../../Config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 interface Props {
   post: IPost;
 }
+
+interface likes {
+  userID: string;
+}
+
 const Post = (props: Props) => {
   const [user] = useAuthState(auth);
   const { post } = props;
-  const [likeAmount, setLikeAmount] = useState<number | null >(null)
+  const [Likes, setLikes] = useState<likes[] | null>(null);
 
   const likesRef = collection(database, "likes");
-  const likesDoc = query(likesRef, where('postID', '==', post.id))
+  const likesDoc = query(likesRef, where("postID", "==", post.id));
 
   const addlikes = async () => {
-    await addDoc(likesRef, {
-      userID: user?.uid,
-      postID: post.id,
-    });
+    try {
+      await addDoc(likesRef, {
+        userID: user?.uid,
+        postID: post.id,
+      });
+
+      if (user) {
+        setLikes((prev) =>
+          prev ? [...prev, { userID: user.uid }] : [{ userID: user.uid }]
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  const likesDisplay = async()=>{
-   const data = await getDocs(likesDoc)
-   setLikeAmount(data.docs.length)
-  }
+  const likesDisplay = async () => {
+    const data = await getDocs(likesDoc);
+    setLikes(data.docs.map((doc) => ({ userID: doc.data().userID })));
+  };
 
-  useEffect(()=>{
-    likesDisplay()
-  },[])
+  const liked = Likes?.find((like) => like.userID === user?.uid);
+
+  useEffect(() => {
+    likesDisplay();
+  }, []);
 
   return (
     <div className="post">
@@ -39,8 +63,11 @@ const Post = (props: Props) => {
       <div>
         <p className="postTextContainer">@ {post.username}</p>
         <p className="postTextContainer">{post.description}</p>
-        <button onClick={addlikes}> &#128077; </button>
-        {likeAmount && <p> Likes: {likeAmount}</p>}
+        <button onClick={addlikes}>
+          {" "}
+          {liked ? <>&#128078;</> : <>&#128077;</>}{" "}
+        </button>
+        {Likes && <p> Likes: {Likes.length}</p>}
       </div>
     </div>
   );
